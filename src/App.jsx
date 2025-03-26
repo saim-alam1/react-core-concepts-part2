cf8fb51eb6fde8b695c49581821d84add0aec456
saim_alam1 <allbehassansaim@gmail.com>
2025-03-27 00:03:19 +0600

    Completed Module

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Batsman from './Batsman';
 import Users from './Users';
 import Friends from './Friends';
 import Posts from './Posts';
+import Players from './Players';
 import { Suspense } from 'react';
 
 
@@ -16,17 +17,17 @@ import { Suspense } from 'react';
 //   return res.json();
 // }
 
-const postFetch = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-  return res.json();
-}
+// const postFetch = async () => {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//   return res.json();
+// }
 
 
 function App() {
 
   // const fetchPromise = fetchFriends();
 
-  const postPromise = postFetch();
+  // const postPromise = postFetch();
 
   function handleClick() {
     alert('I am clicked');
@@ -46,9 +47,11 @@ function App() {
       <h3>Vite + React</h3>
       {/* <button onClick="handleClick()">Click Me</button> */}
 
-      <Suspense fallback={<h3>Posts are coming...</h3>}>
+      {/* <Players></Players> */}
+
+      {/* <Suspense fallback={<h3>Posts are coming...</h3>}>
         <Posts promisePosts={postPromise}></Posts>
-      </Suspense>
+      </Suspense> */}
 
 
       {/* <Suspense fallback={<h3>Loading...</h3>}>
@@ -59,12 +62,12 @@ function App() {
         <Friends promiseRecive={fetchPromise}></Friends>
       </Suspense> */}
 
-      <Batsman></Batsman>
+      {/* <Batsman></Batsman>
 
-      <Counter></Counter>
+      <Counter></Counter> */}
 
 
-      <button onClick={handleClick}>Click Me</button>
+      {/* <button onClick={handleClick}>Click Me</button>
       <button onClick={function handleClick2() {
         alert('I am clicked 2')
       }}>Click Me 2</button>
@@ -74,7 +77,7 @@ function App() {
 
       <button onClick={() => { alert('click 4') }}>Click Me 4</button>
 
-      <button onClick={() => handleAdd5(10)}>Click Add 5</button>
+      <button onClick={() => handleAdd5(10)}>Click Add 5</button> */}
     </>
   )
 }
